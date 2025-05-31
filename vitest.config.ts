3b385788f4e7b2b99f3ffa0ab134b25580826308
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2025-05-31 08:52:52 -0300

    chore: define vitest global

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,10 @@
-import { defineConfig } from 'vitest/config';
-import tsconfigPaths from 'vite-tsconfig-paths';
+import { defineConfig } from 'vitest/config'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
   plugins: [tsconfigPaths()],
   test: {
+    globals: true,
     setupFiles: ['./vitest.setup.ts'],
   },
-});
+})
